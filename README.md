95407b3a4c9884f6b0af0effb238e203d575db97
Mathieu CAROFF <mathieu.caroff@free.fr>
2024-02-16 23:04:19 +0100

    Read a keyboard from the user and display it in a table of tables

--- README.md
+++ README.md
@@ -14,3 +14,35 @@ Read a keyboard from the user and display it in a table of tables
 Detect keypresses and highlight the pressed keys
 
 ## Goal 3
+
+Produce lessons in English adapted to the keyboard of the user
+
+## Goal 4
+
+Write down what the user types as they type it and track errors in its text
+
+## Goal 5
+
+Produce lessons in French-without-accents
+
+## Goal 6
+
+Support secondary symbols on keyboard keys
+
+## Goal 7
+
+Produce lessons in French
+
+## Goal 8
+
+Allow the user to provide their own dictionnary of words from their language
+and produce lessons based off of it.
+
+## Goal 9
+
+Add a mode which adds punctuations `*-=+;:,.!?'"/()[]` to the text.
+
+## Goal 10
+
+Add a mode which adds programmer punctuations `!@#$%^&*-=~_+;:,./?'"\|()[]{}<>`
+to the text.
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,50 @@
-import React from "react"
+import React, { useMemo, useState } from "react"
+import { Keyboard } from "./type"
+
+function processKeyboardText(text: string): Keyboard {
+  return text.split("\n").map((line) => line.split(""))
+}
 
 export function App() {
-  return <div></div>
+  let [keyboardText, setKeyboardText] = useState("")
+  let keyboard = useMemo(
+    () => processKeyboardText(keyboardText),
+    [keyboardText],
+  )
+
+  return (
+    <div>
+      <div>Keyboard Input</div>
+      <div>
+        <textarea
+          value={keyboardText}
+          onChange={(ev) => {
+            setKeyboardText(ev.currentTarget.value)
+          }}
+        />
+      </div>
+      <div>
+        <table className="keyboard">
+          <tbody>
+            {keyboard.map((row, k) => (
+              <tr key={k}>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td className={`keyboard__key keyboard__offset--${k}`} />
+                      {row.map((keycap, m) => (
+                        <td className="keyboard__key" key={m}>
+                          {keycap}
+                        </td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
