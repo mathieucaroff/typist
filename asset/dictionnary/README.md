3d82790b0609fe93bf4ee6a3ce27ced267fdb0c7
Mathieu CAROFF <mathieu.caroff@free.fr>
2024-03-04 02:10:22 +0100

    add builtin language support
    Add word lists for:
    - English
    - French
    - German
    - Spanish

---
+++ asset/dictionnary/README.md
@@ -0,0 +1,8 @@
+# Dictionnary
+
+The word lists come from various places. Theses are:
+
+- [10000 Word list](https://www.mit.edu/~ecprice/wordlist.10000) from ecprice at MIT.
+- [Apéndice:1000 palabras básicas en español](https://es.wiktionary.org/wiki/Ap%C3%A9ndice:1000_palabras_b%C3%A1sicas_en_espa%C3%B1ol) from the Wiktionary project.
+- ["Liste Inconnue"](http://www.3zsoftware.com/fr/listes.php) from 3Z Software.
+- [Bigbossfarin's german word list](https://en.wiktionary.org/wiki/User:Bigbossfarin/10000_German_words) from the Wiktionary project
--- src/components/DictionnarySetter.tsx
+++ src/components/DictionnarySetter.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react"
+import React, { Dispatch, SetStateAction, useState } from "react"
 
 export interface DictionnarySetterProp {
   dictionnary: string[]
@@ -7,13 +7,50 @@ export interface DictionnarySetterProp {
 
 export function DictionnarySetter(prop: DictionnarySetterProp) {
   let { dictionnary, setDictionnary } = prop
+
+  let [loading, setLoading] = useState(false)
+  let [dictionnaryLanguage, setDictionnaryLanguage] = useState("other")
+
   return (
     <div>
+      <select
+        onChange={async (ev) => {
+          setDictionnaryLanguage(ev.currentTarget.value)
+          let name = ev.currentTarget.value.toLowerCase()
+          const dictionnaryTextGetter =
+            {
+              english: () =>
+                import("bundle-text:../../asset/dictionnary/english.list.txt"),
+              espagñol: () =>
+                import("bundle-text:../../asset/dictionnary/espagnol.list.txt"),
+              français: () =>
+                import("bundle-text:../../asset/dictionnary/francais.list.txt"),
+              deutsche: () =>
+                import("bundle-text:../../asset/dictionnary/deutsche.list.txt"),
+            }[name] ?? (async () => dictionnary.join("\n"))
+
+          setLoading(true)
+          let dictionnaryText = await dictionnaryTextGetter()
+          console.log("dictionnaryText", dictionnaryText)
+          setDictionnary(() => {
+            setLoading(false)
+            return dictionnaryText.split("\n")
+          })
+        }}
+        value={dictionnaryLanguage}
+      >
+        <option>other</option>
+        <option>English</option>
+        <option>Espagñol</option>
+        <option>Français</option>
+        <option>Deutsche</option>
+      </select>
       <textarea
         onChange={(ev) => {
           setDictionnary(ev.currentTarget.value.split("\n"))
         }}
         value={dictionnary.join("\n")}
+        disabled={loading}
       ></textarea>
       <input
         type="file"
