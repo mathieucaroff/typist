39265be32fce0ec31372d4ed2c55ccabcaf2025d
Mathieu CAROFF <mathieu.caroff@free.fr>
2024-04-01 07:00:48 +0200

    minor dictionnary setter refactor

--- asset/typeface/README.md
+++ asset/typeface/README.md
@@ -1,4 +1,4 @@
-# snakepony - typeface
+# Typist - typeface
 
 [Roboto](https://www.dafont.com/fr/roboto.font), found on dafont.com.
 [Prisma](https://www.dafont.com/fr/prisma.font), found on dafont.com.
--- src/components/DictionnarySetter.tsx
+++ src/components/DictionnarySetter.tsx
@@ -11,61 +11,72 @@ export function DictionnarySetter(prop: DictionnarySetterProp) {
   let [loading, setLoading] = useState(false)
   let [dictionnaryLanguage, setDictionnaryLanguage] = useState("other")
 
-  return (
-    <div>
-      <select
-        onChange={async (ev) => {
-          setDictionnaryLanguage(ev.currentTarget.value)
-          let name = ev.currentTarget.value.toLowerCase()
-          const dictionnaryTextGetter =
-            {
-              english: () =>
-                import("bundle-text:../../asset/dictionnary/english.list.txt"),
-              espagñol: () =>
-                import("bundle-text:../../asset/dictionnary/espagnol.list.txt"),
-              français: () =>
-                import("bundle-text:../../asset/dictionnary/francais.list.txt"),
-              deutsche: () =>
-                import("bundle-text:../../asset/dictionnary/deutsche.list.txt"),
-            }[name] ?? (async () => dictionnary.join("\n"))
+  const handleDictionnaryLanguageChange = async (
+    ev: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setDictionnaryLanguage(ev.currentTarget.value)
+    let name = ev.currentTarget.value.toLowerCase()
+    const dictionnaryTextGetter =
+      {
+        english: () =>
+          import("bundle-text:../../asset/dictionnary/english.list.txt"),
+        espagñol: () =>
+          import("bundle-text:../../asset/dictionnary/espagnol.list.txt"),
+        français: () =>
+          import("bundle-text:../../asset/dictionnary/francais.list.txt"),
+        deutsche: () =>
+          import("bundle-text:../../asset/dictionnary/deutsche.list.txt"),
+      }[name] ?? (async () => dictionnary.join("\n"))
 
-          setLoading(true)
-          let dictionnaryText = await dictionnaryTextGetter()
-          console.log("dictionnaryText", dictionnaryText)
-          setDictionnary(() => {
-            setLoading(false)
-            return dictionnaryText.split("\n")
-          })
-        }}
-        value={dictionnaryLanguage}
-      >
-        <option>other</option>
-        <option>English</option>
-        <option>Espagñol</option>
-        <option>Français</option>
-        <option>Deutsche</option>
-      </select>
-      <textarea
-        onChange={(ev) => {
-          setDictionnary(ev.currentTarget.value.split("\n"))
-        }}
-        value={dictionnary.join("\n")}
-        disabled={loading}
-      ></textarea>
-      <input
-        type="file"
-        onChange={(ev) => {
-          let { files } = ev.currentTarget
-          if (files && files[0]) {
-            var reader = new FileReader()
-            reader.readAsText(files[0], "utf-8")
-            reader.onload = function (onloadEvent) {
-              setDictionnary((onloadEvent.target!.result as string).split("\n"))
-            }
-          }
-          console.log("file", ev.currentTarget.files![0])
-        }}
-      />
-    </div>
+    setLoading(true)
+    let dictionnaryText = await dictionnaryTextGetter()
+    setDictionnary(() => {
+      setLoading(false)
+      return dictionnaryText.split("\n")
+    })
+  }
+
+  const handleDictionnaryFileChange = (
+    ev: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    let { files } = ev.currentTarget
+    if (files && files[0]) {
+      var reader = new FileReader()
+      reader.readAsText(files[0], "utf-8")
+      setLoading(true)
+      reader.onload = function (onloadEvent) {
+        setDictionnary((onloadEvent.target!.result as string).split("\n"))
+        setLoading(false)
+      }
+    }
+  }
+
+  return (
+    <>
+      <div>Word list</div>
+      <div>
+        <select
+          onChange={handleDictionnaryLanguageChange}
+          value={dictionnaryLanguage}
+        >
+          <option>other</option>
+          <option>English</option>
+          <option>Espagñol</option>
+          <option>Français</option>
+          <option>Deutsche</option>
+        </select>
+        <input type="file" onChange={handleDictionnaryFileChange} />
+      </div>
+      <div>
+        <textarea
+          onChange={(ev) => {
+            setDictionnary(ev.currentTarget.value.split("\n"))
+            setDictionnaryLanguage("other")
+          }}
+          value={dictionnary.join("\n")}
+          disabled={loading}
+        ></textarea>
+      </div>
+    </>
   )
 }
